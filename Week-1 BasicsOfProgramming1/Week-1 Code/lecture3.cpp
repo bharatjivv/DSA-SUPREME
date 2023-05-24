a0d78e9f9604d6cfe129216bdc1a254056e6ea6d
BharatTate <hibharatj@gmail.com>
2023-05-24 14:06:53 +0530

    Rearranged

---
+++ Week-1 BasicsOfProgramming1/Week-1 Code/lecture3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // int bronum;
+    // cout << "Enter bronum value : ";
+    // cin >> bronum;
+
+    // if(bronum==0){
+    //     cout << "Baat ban jaygi";
+    // }
+    // else{
+    //     cout << "Baat nahi banegi";
+    // }
+
+    for(int i=1; i<=10; i++){
+        cout << "13 * " << i << " = " << 13*i << endl;
+    }
+}
